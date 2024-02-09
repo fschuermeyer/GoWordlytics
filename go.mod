179dfd9ce2bbd98e4b5c7c94322bb10825821d57
Felix Schürmeyer <fschuermeyer@gmail.com>
2024-02-09 22:02:25 +0100

    Add dependencies and update versioning logic

--- go.mod
+++ go.mod
@@ -3,8 +3,13 @@ module github.com/fschuermeyer/GoWordlytics
 go 1.21.0
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.1
+	golang.org/x/net v0.20.0
+)
+
+require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/cobra v1.8.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	golang.org/x/net v0.20.0
 )
--- internal/analyze/version.go
+++ internal/analyze/version.go
@@ -1,5 +1,51 @@
 package analyze
 
+import (
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
 func (a *Analyze) version(url string) string {
+	var version string
+
+	if len(a.data.htmlIndex) != 0 {
+		doc, err := goquery.NewDocumentFromReader(strings.NewReader(a.data.htmlIndex))
+
+		if err != nil {
+			version = a.versionByMetaTag(doc)
+		}
+
+		if err != nil && len(version) == 0 {
+			version = a.versionByEnquedScripts(doc)
+		}
+	}
+
+	return "1.0"
+}
+
+func (a *Analyze) versionByMetaTag(doc *goquery.Document) string {
+	s := doc.Find("meta[name=generator]").First()
+
+	value := strings.TrimSpace(s.AttrOr("content", ""))
+
+	for _, indicator := range a.vIndicatorsMetaTag {
+		if strings.HasPrefix(value, indicator.indicator) {
+			return strings.ReplaceAll(value, indicator.indicator, "")
+		}
+	}
+
+	return ""
+}
+
+func (a *Analyze) versionByEnquedScripts(doc *goquery.Document) string {
+	return "1.0"
+}
+
+func (a *Analyze) versionByRssFeed() string {
+	return "1.0"
+}
+
+func (a *Analyze) versionByLoginPage() string {
 	return "1.0"
 }
